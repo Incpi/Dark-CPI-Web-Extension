dee19c01ffb5b9fd793946953334f022a5ca2e74
Om <87596092+incpi@users.noreply.github.com>
2024-06-01 20:32:42 +0530

    UI Fix

--- popup/popup.js
+++ popup/popup.js
@@ -1,16 +1,47 @@
-document.getElementById('options').addEventListener('change', function () {
-    var value = this.options[this.selectedIndex].value;
-    chrome.storage.sync.set({ 'SapDarkCPITheme': value }, function () {
-        if (chrome.runtime.lastError) {
-            console.error('Error setting option:', chrome.runtime.lastError);
-        } else {
-            console.log('Option set to:', value);
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.btn');
+    const versionElements = document.querySelectorAll('.version');
+    const activetheme = document.querySelector('.activetheme');
+    const body = document.body;
+
+    // Set the version in all elements with class 'version'
+    versionElements.forEach((e) => {
+        e.innerHTML = chrome.runtime.getManifest().version;
+    });
+
+    // Event listener for button clicks
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            buttons.forEach(btn => btn.classList.remove('active'));
+            this.classList.add('active');
+            activetheme.textContent = this.textContent;
+            const selectedValue = this.getAttribute('data-value');
+
+            // Store the selected theme and toggle dark theme class
+            chrome.storage.sync.set({ "SapDarkCPITheme": selectedValue }, function () {
+                console.log('Theme selected:', selectedValue);
+                if (selectedValue === "1") {
+                    body.classList.add("dark-theme");
+                } else {
+                    body.classList.remove("dark-theme");
+                }
+            });
+        });
+    });
+
+    // Load the selected theme from Chrome storage
+    chrome.storage.sync.get(['SapDarkCPITheme'], function (result) {
+        if (result.SapDarkCPITheme) {
+            const selectedButton = document.querySelector(`.btn[data-value="${result.SapDarkCPITheme}"]`);
+            if (selectedButton) {
+                selectedButton.classList.add('active');
+                activetheme.textContent = selectedButton.textContent;
+                if (result.SapDarkCPITheme === "1") {
+                    body.classList.add("dark-theme");
+                } else {
+                    body.classList.remove("dark-theme");
+                }
+            }
         }
     });
 });
-
-chrome.storage.sync.get(["SapDarkCPITheme"], (result) => {
-    document.getElementById('options').value = result["SapDarkCPITheme"] || 0;
-});
-
-document.querySelectorAll('.version').forEach((e)=>e.innerHTML =chrome.runtime.getManifest().version)
