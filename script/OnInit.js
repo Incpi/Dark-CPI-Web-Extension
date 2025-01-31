ee5c54b0486a3b9d18eb0ad100fe39af3ce0228e
Omkar <OMK14p@outlook.com>
2025-02-01 03:07:03 +0530

    v 2.5.0 - Beta test

--- script/OnInit.js
+++ script/OnInit.js
@@ -2,89 +2,89 @@
 console.log("Initializing OnInit.js...");
 
 function setupChromeApiListener() {
-  window.addEventListener("requestDataChromeApi", (event) => {
-    if (event.detail?.request === "chromeExtensionURL") {
-      try {
-        const responseEvent = new CustomEvent("responseDataChromeApi", {
-          detail: { success: true, url: chrome.runtime.getURL("/"), version: chrome.runtime.getManifest().version },
-        });
-        window.dispatchEvent(responseEvent);
-      } catch (error) {
-        console.error("Error fetching Chrome extension URL:", error);
-        const responseEvent = new CustomEvent("responseDataChromeApi", {
-          detail: { success: false, message: error.message },
-        });
-        window.dispatchEvent(responseEvent);
-      }
-    }
-  });
-  window.addEventListener("getManifest", (event) => {
-    if (event.detail?.request === "getManifest") {
-      try {
-        const responseEvent = new CustomEvent("getManifest", {
-          detail: {
-            success: true,
-            version: chrome.runtime.getManifest().version,
-          },
-        });
-        window.dispatchEvent(responseEvent);
-      } catch (error) {
-        console.error("Error fetching Chrome extension Version:", error);
-        const responseEvent = new CustomEvent("getManifest", {
-          detail: { success: false, message: error.message },
-        });
-        window.dispatchEvent(responseEvent);
-      }
-    }
-  });
+	window.addEventListener("requestDataChromeApi",(event) => {
+		if (event.detail?.request === "chromeExtensionURL") {
+			try {
+				const responseEvent = new CustomEvent("responseDataChromeApi",{
+					detail:{ success:true,url:chrome.runtime.getURL("/"),version:chrome.runtime.getManifest().version },
+				});
+				window.dispatchEvent(responseEvent);
+			} catch (error) {
+				console.error("Error fetching Chrome extension URL:",error);
+				const responseEvent = new CustomEvent("responseDataChromeApi",{
+					detail:{ success:false,message:error.message },
+				});
+				window.dispatchEvent(responseEvent);
+			}
+		}
+	});
+	window.addEventListener("getManifest",(event) => {
+		if (event.detail?.request === "getManifest") {
+			try {
+				const responseEvent = new CustomEvent("getManifest",{
+					detail:{
+						success:true,
+						version:chrome.runtime.getManifest().version,
+					},
+				});
+				window.dispatchEvent(responseEvent);
+			} catch (error) {
+				console.error("Error fetching Chrome extension Version:",error);
+				const responseEvent = new CustomEvent("getManifest",{
+					detail:{ success:false,message:error.message },
+				});
+				window.dispatchEvent(responseEvent);
+			}
+		}
+	});
 }
 
 function getSubstringBeforeDot(url) {
-  return url.substring(0, url.indexOf("."));
+	return url.substring(0,url.indexOf("."));
 }
 
 function setupStorageListener() {
-  const id = getSubstringBeforeDot(window.location.hostname);
+	const id = getSubstringBeforeDot(window.location.hostname);
 
-  window.addEventListener("storeData", (event) => {
-    const { key, value } = event.detail;
-    console.log("Content Script: Storing data for key:", key);
+	window.addEventListener("storeData",(event) => {
+		const { key,value } = event.detail;
+		// console.log("Content Script: Storing data for key:", key);
 
-    try {
-      chrome.storage.sync.get([id], (result) => {
-        const storageData = result[id] || {}; // Ensure object exists
-        storageData[key] = JSON.stringify(value); // Store data
+		try {
+			chrome.storage.sync.get([id],(result) => {
+				const storageData = result[id] || {}; // Ensure object exists
+				storageData[key] = JSON.stringify(value); // Store data
 
-        chrome.storage.sync.set({ [id]: storageData }, () => {
-          const responseEvent = new CustomEvent("storeDataResponse", {
-            detail: { success: true, message: `Key "${key}" stored successfully.` },
-          });
-          window.dispatchEvent(responseEvent);
-        });
-      });
-    } catch (error) {
-      console.error("Content Script: Error storing data", error);
-      const responseEvent = new CustomEvent("storeDataResponse", {
-        detail: { success: false, message: error.message },
-      });
-      window.dispatchEvent(responseEvent);
-    }
-  });
+				chrome.storage.sync.set({ [id]:storageData },() => {
+					const responseEvent = new CustomEvent("storeDataResponse",{
+						detail:{ success:true,message:`Key "${key}" stored successfully.` },
+					});
+					window.dispatchEvent(responseEvent);
+				});
+			});
+		} catch (error) {
+			console.error("Content Script: Error storing data",error);
+			const responseEvent = new CustomEvent("storeDataResponse",{
+				detail:{ success:false,message:error.message },
+			});
+			window.dispatchEvent(responseEvent);
+		}
+	});
 
-  window.addEventListener("getData", (event) => {
-    const { key } = event.detail;
-    console.log("Content Script: Retrieving data for key:", key);
+	window.addEventListener("getData",(event) => {
+		const { key } = event.detail;
+		// console.log("Content Script: Retrieving data for key:", key);
 
-    chrome.storage.sync.get([id], (result) => {
-      const storageData = result[id] || {}; // Ensure object exists
-      const value = storageData[key] ? JSON.parse(storageData[key]) : null;
+		chrome.storage.sync.get([id],(result) => {
+			const storageData = result[id] || {}; // Ensure object exists
+			const value = storageData[key] ? JSON.parse(storageData[key]) : null;
 
-      const responseEvent = new CustomEvent("getDataResponse", {
-        detail: { success: value !== null, value },
-      });
-      window.dispatchEvent(responseEvent);
-    });
-  });
+			const responseEvent = new CustomEvent("getDataResponse",{
+				detail:{ success:value !== null,value },
+			});
+			window.dispatchEvent(responseEvent);
+		});
+	});
 }
 
 setupChromeApiListener();
--- script/utils/const_new.js
+++ script/utils/const_new.js
@@ -1,56 +1,56 @@
-sap.ui.define([], function () {
-  "use strict";
-  return {
-    data_update_label: { bugFixes: "Bug Fixes", improvements: "Improvements", features: "Features" },
-    data_updates: {
-      bugFixes: [
-        { description: "Resolved CSS issues preventing the scrollable section in the configuration window from functioning correctly." },
-        { description: "Fixed overflow and text clipping issues in the trace, logs, and design pages, especially in dark mode, ensuring a seamless user experience." },
-      ],
-      features: [
-        { description: "Introduced a quick message popup feature, similar to the CPI helper popup, for enhanced user communication." },
-        { description: "Added an automatic trace activation on expiry in Power Trace for more efficient monitoring." },
-        { description: "Inline payload formatting is now available in the trace view for clearer data presentation." },
-        { description: "Quick links to the trace page and log monitoring page have been implemented for easier navigation." },
-        { description: "Table sorting and filtering are now standard UI features, improving data accessibility and usability." },
-      ],
-      improvements: [
-        { description: "Optimized the extension runtime, significantly reducing load times for better performance." },
-        { description: "Reduced the extension size by minimizing dependencies and optimizing external libraries." },
-        { description: "Limited style overrides to ensure a more consistent and native user experience." },
-      ],
-    },
-    author: {
-      linkdin: "https://linkedin.com/in/incpi",
-      github: "https://github.com/incpi",
-      githubrepo: "https://github.com/incpi/Dark-CPI-Web-Extension",
-      sapblog: "https://community.sap.com/t5/technology-blogs-by-members/introducing-dark-cpi-web-extension-for-sap-applications/bc-p/13860794",
-    },
-    noticeType: ["Warning", "Error", "Success", "Information", "None"],
-    notice: [
-      // {text: `All control has been moved from the popup to the webpage header - "DC" icon.`, type: "Warning | Error", showIcon: true, visible: true,}
-    ],
-    neededLinks: [
-      {
-        label: "List of Internal Headers and Properties",
-        link: "https://help.sap.com/docs/cloud-integration/sap-cloud-integration/headers-and-exchange-properties-provided-by-integration-framework?trk=feed-detail_comments-list_comment-text",
-      },
-      {
-        label: "Groovy IDE",
-        link: "https://groovyide.com/cpi",
-      },
-      {
-        label: "Convita IDE",
-        link: "https://ide.contiva.com/",
-      },
-      {
-        label: "SAP CPI Help",
-        link: "https://help.sap.com/docs/cloud-integration?locale=en-US",
-      },
-    ],
-    footer: {
-      label: "Report Issues",
-      Link: "https://github.com/incpi/Dark-CPI-Web-Extension/issues",
-    },
-  };
+sap.ui.define([],function() {
+	"use strict";
+	return {
+		data_update_label:{ bugFixes:"Bug Fixes",improvements:"Improvements",features:"Features" },
+		data_updates:{
+			bugFixes:[
+				{ description:"Resolved CSS issues preventing the scrollable section in the configuration window from functioning correctly." },
+				{ description:"Fixed overflow and text clipping issues in the trace, logs, and design pages, especially in dark mode, ensuring a seamless user experience." },
+			],
+			features:[
+				{ description:"Introduced a quick message popup feature, similar to the CPI helper popup, for enhanced user communication." },
+				{ description:"Added an automatic trace activation on expiry in Power Trace for more efficient monitoring." },
+				{ description:"Inline payload formatting is now available in the trace view for clearer data presentation." },
+				{ description:"Quick links to the trace page and log monitoring page have been implemented for easier navigation." },
+				{ description:"Table sorting and filtering are now standard UI features, improving data accessibility and usability." },
+			],
+			improvements:[
+				{ description:"Optimized the extension runtime, significantly reducing load times for better performance." },
+				{ description:"Reduced the extension size by minimizing dependencies and optimizing external libraries." },
+				{ description:"Limited style overrides to ensure a more consistent and native user experience." },
+			],
+		},
+		author:{
+			linkdin:"https://linkedin.com/in/incpi",
+			github:"https://github.com/incpi",
+			githubrepo:"https://github.com/incpi/Dark-CPI-Web-Extension",
+			sapblog:"https://community.sap.com/t5/technology-blogs-by-members/introducing-dark-cpi-web-extension-for-sap-applications/bc-p/13860794",
+		},
+		noticeType:["Warning","Error","Success","Information","None"],
+		notice:[
+			// {text: `All control has been moved from the popup to the webpage header - "DC" icon.`, type: "Warning | Error", showIcon: true, visible: true,}
+		],
+		neededLinks:[
+			{
+				label:"List of Internal Headers and Properties",
+				link:"https://help.sap.com/docs/cloud-integration/sap-cloud-integration/headers-and-exchange-properties-provided-by-integration-framework?trk=feed-detail_comments-list_comment-text",
+			},
+			{
+				label:"Groovy IDE",
+				link:"https://groovyide.com/cpi",
+			},
+			{
+				label:"Convita IDE",
+				link:"https://ide.contiva.com/",
+			},
+			{
+				label:"SAP CPI Help",
+				link:"https://help.sap.com/docs/cloud-integration?locale=en-US",
+			},
+		],
+		footer:{
+			label:"Report Issues",
+			Link:"https://github.com/incpi/Dark-CPI-Web-Extension/issues",
+		},
+	};
 });
